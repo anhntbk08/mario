bb4d8104305ed65519a4f466c4670cdfdfe65b77
thanh le <lethanhbk55@gmail.com>
2016-04-05 23:45:38 +0700

    update classpath order and export nhbmessaging

--- Mario/sbin/start.sh
+++ Mario/sbin/start.sh
@@ -53,7 +53,7 @@ GC_TUNE="-XX:+UseParNewGC \
 
 JVM_OPTS="$GC_TUNE \
 -server \
--d64 -Xms4g -Xmx4g \
+-d64 -Xms512M -Xmx512M \
 -XX:+HeapDumpOnOutOfMemoryError \
 -XX:HeapDumpPath='logs/dump-$(date +%Y%m%d%H%M%S).hprof' \
 -Dfile.encoding=UTF-8 \
